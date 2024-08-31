bbac9ca606b9296861a16e45bb0208a169ff6111
rahimz <rahimz@kth.se>
2024-08-31 18:18:32 +0200

    second

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+import java.io.File
+import java.io.InputStream
+import java.util.*
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    var fileName = args[0]
+    val inputStream: InputStream = File(fileName).inputStream()
+    val queue: Queue<Char> = LinkedList()
+    var number = inputStream.bufferedReader().read()
+    if (number == -1) {
+        println("Invalid JSON, empty file.")
+        exitProcess(1)
+    }
+    while (number != -1) {
+        if (number.toChar() == '{') {
+            queue.add(number.toChar())
+        }
+        if ((number.toChar()) == '}' && queue.peek() != '{') {
+            println("Error: Mismatched closing brace '}' found.")
+            exitProcess(1)
+        }
+        if ((number.toChar()) == '}' && queue.peek() == '{') {
+            queue.add(number.toChar())
+        }
+        number = inputStream.bufferedReader().read()
+    }
 }
