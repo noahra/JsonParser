abb2eb036a609a50452932ec8dd19dfbc674c994
rahimz <rahimz@kth.se>
2024-09-02 15:34:52 +0200

    wip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,29 +2,11 @@ package org.example
 
 import java.io.File
 import java.io.InputStream
-import java.util.*
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    var fileName = args[0]
+    val fileName = args[0]
     val inputStream: InputStream = File(fileName).inputStream()
-    val queue: Queue<Char> = LinkedList()
-    var number = inputStream.bufferedReader().read()
-    if (number == -1) {
-        println("Invalid JSON, empty file.")
-        exitProcess(1)
-    }
-    while (number != -1) {
-        if (number.toChar() == '{') {
-            queue.add(number.toChar())
-        }
-        if ((number.toChar()) == '}' && queue.peek() != '{') {
-            println("Error: Mismatched closing brace '}' found.")
-            exitProcess(1)
-        }
-        if ((number.toChar()) == '}' && queue.peek() == '{') {
-            queue.add(number.toChar())
-        }
-        number = inputStream.bufferedReader().read()
-    }
+    val list = Tokenizer().tokenize(inputStream)
+
+    println(list)
 }
--- src/main/kotlin/Tokenizer.kt
+++ src/main/kotlin/Tokenizer.kt
@@ -6,8 +6,8 @@ import kotlin.system.exitProcess
 class Tokenizer {
 
     fun tokenize(stream: InputStream): List<Any> {
-
-        var number = stream.bufferedReader().read()
+        val reader = stream.bufferedReader()
+        var number = reader.read()
         val listOfTokens: MutableList<Any> = mutableListOf()
         if (number == -1) {
             println("Invalid JSON, empty file.")
@@ -15,29 +15,43 @@ class Tokenizer {
         }
         while (number != -1) {
             if (number.toChar() == '{') {
-                listOfTokens.add(Token(TokenType.LEFT_CURLY_BRACE, number.toString()))
+                listOfTokens.add(Token(TokenType.LEFT_CURLY_BRACE, number.toChar()))
             }
             if ((number.toChar()) == '}') {
-                listOfTokens.add(Token(TokenType.RIGHT_CURLY_BRACE, number.toString()))
+                listOfTokens.add(Token(TokenType.RIGHT_CURLY_BRACE, number.toChar()))
+
             }
             if ((number.toChar()) == '[') {
-                listOfTokens.add(Token(TokenType.LEFT_SQUARE_BRACKET, number.toString()))
+                listOfTokens.add(Token(TokenType.LEFT_SQUARE_BRACKET, number.toChar()))
             }
             if ((number.toChar()) == ']') {
-                listOfTokens.add(Token(TokenType.RIGHT_SQUARE_BRACKET, number.toString()))
+                listOfTokens.add(Token(TokenType.RIGHT_SQUARE_BRACKET, number.toChar()))
             }
             if ((number.toChar()) == ':') {
-                listOfTokens.add(Token(TokenType.COLON, number.toString()))
+                listOfTokens.add(Token(TokenType.COLON, number.toChar()))
+            }
+            if (number.toChar() == '"') {
+                val charArray = mutableListOf<Char>()
+                number = reader.read()
+                while (number.toChar() != '"') {
+                    charArray.add(number.toChar())
+                    number = reader.read()
+                }
+                if (charArray.isNotEmpty()) {
+                    val str = charArray.joinToString("")
+                    listOfTokens.add(Token(TokenType.STRING, str))
+                }
+
             }
             if ((number.toChar()).isDigit()) {
-                var charArray = mutableListOf<Char>()
+                val charArray = mutableListOf<Char>()
                 charArray.add(number.toChar())
-                stream.bufferedReader().mark(1)
-                number = stream.bufferedReader().read()
+                reader.mark(1)
+                number = reader.read()
                 while (number.toChar().isDigit() || number.toChar() == '.') {
                     charArray.add(number.toChar())
-                    stream.bufferedReader().mark(1)
-                    number = stream.bufferedReader().read()
+                    reader.mark(1)
+                    number = reader.read()
                 }
                 if (charArray.isNotEmpty()) {
                     val numberString = charArray.joinToString("")
@@ -49,7 +63,8 @@ class Tokenizer {
                     listOfTokens.add(Token(TokenType.NUMBER, numberToAdd))
                 }
             }
-            number = stream.bufferedReader().read()
+            number = reader.read()
+
         }
         return listOfTokens
     }
